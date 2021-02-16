ccf5f2f0a8b3a89ede4402d5a843d1fb3df147fa
skv93-coder <skk89879@gmail.com>
2021-02-16 23:10:20 +0530

    Layout-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
-import { Menu, Tabs, Card, Col, Row, Layout } from "antd";
+import {
+  Menu,
+  Tabs,
+  Card,
+  Col,
+  Row,
+  Layout,
+  PageHeader,
+  List,
+  Divider,
+} from "antd";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+//const data =
+const data = new Array(40).fill("shu");
+
 function App() {
   const { TabPane } = Tabs;
   const { SubMenu } = Menu;
+  const routes = [
+    {
+      path: "index",
+      breadcrumbName: "First-level Menu",
+    },
+    {
+      path: "first",
+      breadcrumbName: "Second-level Menu",
+    },
+    {
+      path: "second",
+      breadcrumbName: "Third-level Menu",
+    },
+  ];
+
   const gridStyle = {
     width: "25%",
     textAlign: "center",
@@ -12,76 +40,103 @@ function App() {
   const { Content, Sider } = Layout;
   return (
     <>
-      <Layout>
-        <Sider width={200}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-          >
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <Tabs defaultActiveKey="1">
-              <TabPane tab="Tab 1" key="1">
-                <div className="site-card-wrapper">
-                  <Row gutter={16}>
-                    <Col span={8}>
-                      <Card title="Card title" bordered={false}>
-                        <p>Card Content</p>
-                        <p>Card Content</p>
+      <PageHeader
+        className="site-page-header"
+        title="Title"
+        breadcrumb={{ routes }}
+        subTitle="This is a subtitle"
+      />
 
-                        <p>Card Content</p>
-                      </Card>
-                    </Col>
-                    <Col span={8}>
-                      <Card title="Card title" bordered={false}>
-                        <p>Card Content</p>
-                        <p>Card Content</p>
-                        <p>Card Content</p>{" "}
-                      </Card>
-                    </Col>
-                    <Col span={8}>
-                      <Card title="Card title" bordered={false}>
-                        <p>Card Content</p>
-                        <p>Card Content</p>
+      <Sider
+        width={200}
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+        }}
+      >
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%", borderRight: 0 }}
+          theme="light"
+        >
+          {/* {[
+            "akao",
+            "a",
+            "b",
+            "c",
+            "shu",
+            "jsisjiijs",
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            12,
+            34,
+            56,
+            78,
+          ].map((i) => (
+            <>
+              {" "}
+              <Menu.Item key={i}>Option 1</Menu.Item>
+            </>
+          ))} */}
+          <List
+            size="small"
+            bordered
+            dataSource={data}
+            renderItem={(item) => <List.Item>{item}</List.Item>}
+          />
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Tab 1" key="1">
+              <div className="site-card-wrapper">
+                <Row gutter={16}>
+                  <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                      <p>Card Content</p>
+                      <p>Card Content</p>
 
-                        <p>Card Content</p>
-                      </Card>
-                    </Col>
-                  </Row>
-                </div>
-                ,
-              </TabPane>
-              <TabPane tab="Tab 2" key="2">
-                Content of Tab Pane 2
-              </TabPane>
-              <TabPane tab="Tab 3" key="3">
-                Content of Tab Pane 3
-              </TabPane>
-            </Tabs>
-          </Content>
-        </Layout>
+                      <p>Card Content</p>
+                    </Card>
+                  </Col>
+                  <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                      <p>Card Content</p>
+                      <p>Card Content</p>
+                      <p>Card Content</p>{" "}
+                    </Card>
+                  </Col>
+                  <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                      <p>Card Content</p>
+                      <p>Card Content</p>
+
+                      <p>Card Content</p>
+                    </Card>
+                  </Col>
+                </Row>
+              </div>
+              ,
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+        </Content>
       </Layout>
     </>
   );
