c9a82da2a6a03a98be1e54dc375e3bc208bb20fa
skv93-coder <skk89879@gmail.com>
2021-02-18 21:18:02 +0530

    layout-2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "antd/dist/antd.css";
+
 import {
   Menu,
   Tabs,
@@ -11,135 +13,55 @@ import {
   Divider,
 } from "antd";
 
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-//const data =
-const data = new Array(40).fill("shu");
+const { Sider } = Layout;
 
-function App() {
-  const { TabPane } = Tabs;
-  const { SubMenu } = Menu;
-  const routes = [
-    {
-      path: "index",
-      breadcrumbName: "First-level Menu",
-    },
-    {
-      path: "first",
-      breadcrumbName: "Second-level Menu",
-    },
-    {
-      path: "second",
-      breadcrumbName: "Third-level Menu",
-    },
+export default function App() {
+  const data = [
+    "Racing car sprays burning fuel into crowd.",
+    "Japanese princess to wed commoner.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
   ];
 
-  const gridStyle = {
-    width: "25%",
-    textAlign: "center",
-  };
-  const { Content, Sider } = Layout;
   return (
     <>
-      <PageHeader
-        className="site-page-header"
-        title="Title"
-        breadcrumb={{ routes }}
-        subTitle="This is a subtitle"
-      />
+      <div>
+        <div>
+          <Row>
+            <Col span={6}>
+              <Sider
+                className="sider-back"
+                style={{
+                  overflow: "auto",
+                  height: "100vh",
+                  position: "fixed",
+                  left: 0,
+                  color: "white",
+                }}
+              >
+                <List
+                  header={<div>Header</div>}
+                  footer={<div>Footer</div>}
+                  bordered
+                  dataSource={data}
+                  renderItem={(item) => <List.Item>{item}</List.Item>}
+                />
+              </Sider>
+            </Col>
 
-      <Sider
-        width={200}
-        style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-        }}
-      >
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["sub1"]}
-          style={{ height: "100%", borderRight: 0 }}
-          theme="light"
-        >
-          {/* {[
-            "akao",
-            "a",
-            "b",
-            "c",
-            "shu",
-            "jsisjiijs",
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            12,
-            34,
-            56,
-            78,
-          ].map((i) => (
-            <>
-              {" "}
-              <Menu.Item key={i}>Option 1</Menu.Item>
-            </>
-          ))} */}
-          <List
-            size="small"
-            bordered
-            dataSource={data}
-            renderItem={(item) => <List.Item>{item}</List.Item>}
-          />
-        </Menu>
-      </Sider>
-      <Layout className="site-layout" style={{ marginLeft: 200 }}>
-        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-          <Tabs defaultActiveKey="1">
-            <TabPane tab="Tab 1" key="1">
-              <div className="site-card-wrapper">
-                <Row gutter={16}>
-                  <Col span={8}>
-                    <Card title="Card title" bordered={false}>
-                      <p>Card Content</p>
-                      <p>Card Content</p>
-
-                      <p>Card Content</p>
-                    </Card>
-                  </Col>
-                  <Col span={8}>
-                    <Card title="Card title" bordered={false}>
-                      <p>Card Content</p>
-                      <p>Card Content</p>
-                      <p>Card Content</p>{" "}
-                    </Card>
-                  </Col>
-                  <Col span={8}>
-                    <Card title="Card title" bordered={false}>
-                      <p>Card Content</p>
-                      <p>Card Content</p>
-
-                      <p>Card Content</p>
-                    </Card>
-                  </Col>
-                </Row>
-              </div>
-              ,
-            </TabPane>
-            <TabPane tab="Tab 2" key="2">
-              Content of Tab Pane 2
-            </TabPane>
-            <TabPane tab="Tab 3" key="3">
-              Content of Tab Pane 3
-            </TabPane>
-          </Tabs>
-        </Content>
-      </Layout>
+            <Col span={18}>
+              <div style={{ height: "100vh", display: "flex" }}>This</div>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </>
   );
 }
-
-export default App;
